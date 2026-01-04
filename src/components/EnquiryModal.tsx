2e5fcdca45ef442cbd369aee71bcc8b5cc6ff14b
nandandarak <146520876+nandandarak@users.noreply.github.com>
2026-01-04 16:09:57 +0530

    Update EnquiryModal.tsx

--- src/components/EnquiryModal.tsx
+++ src/components/EnquiryModal.tsx
@@ -84,8 +84,8 @@ const EnquiryModal = ({ isOpen, onClose, destination }: EnquiryModalProps) => {
   src={formUrl}
   className="w-full h-full border-0"
   style={{
-    marginTop: "-280px",
-    height: "calc(100% + 280px)",
+    marginTop: "-270px",
+    height: "calc(100% + 270px)",
   }}
   title="Enquiry Form"
 />
