456fb0edaa64643ea80e715b996551ec274eb4b1
nandandarak <146520876+nandandarak@users.noreply.github.com>
2026-01-06 20:41:59 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -37,7 +37,7 @@ const domesticCards = [
   {
   name: "Kashmir",
   image:
-    "https://images.unsplash.com/photo-1609947017136-9daf32a5eb16?auto=format&fit=crop&w=900&q=80",
+    "https://images.unsplash.com/photo-1587732155578-6a7359f11227?auto=format&fit=crop&w=900&q=80",
 },
   {
     name: "Kerala",
